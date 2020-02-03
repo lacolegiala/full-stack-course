21c4ae33cbacd999aa49bd785ce263a8be436935
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-02-03 18:40:58 +0200

    Make a base for the phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import './App.css';
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1  
+    }
+  
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    console.log(nameObject)
+    console.log(persons)
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input
+           value={newName}
+           onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button onClick={addName} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      </ul>
+    </div>
+  )
+
+}
+export default App;
