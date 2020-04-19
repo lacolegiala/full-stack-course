82c359676d501b5986c62c0b3c67533895dc2185
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-04-19 18:46:19 +0300

    Fix indentation

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -5,31 +5,31 @@ const App = (props) => {
   const [votes, setVotes] = useState(props.points)
 
   return (
-    
     <div>
       <h2>Anecdote of the day</h2>
-      <Button onClick={() => {
-        setSelected(randomNumber(props.anecdotes.length))
-      }
-      }
-      text='Next anecdote'>
-      </Button> 
+      <Button
+        onClick={() => {
+          setSelected(randomNumber(props.anecdotes.length))
+        }}
+        text='Next anecdote'
+      />
       <div>
-        <Button onClick={() => {
-          const copyOfVotes = [...votes]
-          copyOfVotes[selected] += 1
-          setVotes(copyOfVotes)
-          console.log("Voted ", copyOfVotes[selected])
-        }
-        } 
-        text='Vote'></Button>
+        <Button 
+          onClick={() => {
+            const copyOfVotes = [...votes]
+            copyOfVotes[selected] += 1
+            setVotes(copyOfVotes)
+            console.log("Voted ", copyOfVotes[selected])
+          }} 
+          text='Vote'
+        />
         This has {votes[selected]} votes
       </div>
       <div>
         {props.anecdotes[selected]}
       </div>
       <h2>Anecdote with most votes</h2>
-        <div>{props.anecdotes[mostVotes(votes)]}</div>
+      <div>{props.anecdotes[mostVotes(votes)]}</div>
     </div>
   )
 }
