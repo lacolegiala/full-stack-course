b51dff89a73237c1a0771a48fe3c101d46f5181b
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-25 16:12:13 +0200

    Fix indentation

--- part8New/library/library-frontend/src/queries.js
+++ part8New/library/library-frontend/src/queries.js
@@ -1,37 +1,37 @@
 import { gql  } from '@apollo/client'
 
 export const ALL_AUTHORS = gql`
-query {
-  allAuthors {
-    name
-    born
-    bookCount
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
   }
-}
 `
 
 export const ALL_BOOKS = gql`
-query {
-  allBooks {
-    title,
-    author,
-    published
+  query {
+    allBooks {
+      title,
+      author,
+      published
+    }
   }
-}
 `
 export const ADD_BOOK = gql`
-mutation addBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
-  addBook(
-    title: $title,
-    published: $published,
-    author: $author,
-    genres: $genres
-  ) {
-    title
-    published
-    author
-    genres
-    id
+  mutation addBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      published: $published,
+      author: $author,
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      genres
+      id
+    }
   }
-}
 `
