6c32c0f0de240147e70372b6e2a8b63354961fd0
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-25 10:59:45 +0200

    List author data

--- part8New/library/library-frontend/src/components/Authors.js
+++ part8New/library/library-frontend/src/components/Authors.js
@@ -1,15 +1,31 @@
   
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
 
 const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+  
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (authors.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
-      <h2>authorsss</h2>
+      <h2>authors</h2>
       <table>
         <tbody>
           <tr>
@@ -21,7 +37,7 @@ const Authors = (props) => {
               books
             </th>
           </tr>
-          {authors.map(a =>
+          {authors.data.allAuthors.map(a =>
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
