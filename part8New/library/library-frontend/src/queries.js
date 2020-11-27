b0990e435bacf4dd60a3f97544dd2577df8ffc47
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-27 16:33:58 +0200

    Make frontend work with modified database

--- part8New/library/library-frontend/src/queries.js
+++ part8New/library/library-frontend/src/queries.js
@@ -5,7 +5,6 @@ export const ALL_AUTHORS = gql`
     allAuthors {
       name
       born
-      bookCount
     }
   }
 `
@@ -14,7 +13,6 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title,
-      author,
       published
     }
   }
@@ -29,7 +27,6 @@ export const ADD_BOOK = gql`
     ) {
       title
       published
-      author
       genres
       id
     }
