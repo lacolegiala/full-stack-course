db55cca73f2f07d82d1ee7d6373aca8a6ceb5306
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-12-20 15:52:18 +0200

    Make first version of login feature

--- part8New/library/library-frontend/src/App.js
+++ part8New/library/library-frontend/src/App.js
@@ -3,9 +3,12 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Login from './components/Login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
 
   return (
     <div>
@@ -13,7 +16,10 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage('login')}>login</button>
       </div>
+
+      {token && <div>Logged in</div>}
 
       <Authors
         show={page === 'authors'}
@@ -25,6 +31,12 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <Login
+        setToken={setToken}
+        show={page === 'login'}
+        
       />
 
     </div>
--- part8New/library/library-frontend/src/queries.js
+++ part8New/library/library-frontend/src/queries.js
@@ -47,3 +47,13 @@ export const EDIT_AUTHOR = gql`
     }
   }
 `
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(
+      username: $username
+      password: $password
+    ) {
+      value
+    }
+  }
+`
