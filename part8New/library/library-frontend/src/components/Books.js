c4e92c9e0973e0608168784259c01aa9e0ddc40d
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-12-22 17:54:50 +0200

    Make a book filtering feature

--- part8New/library/library-frontend/src/components/Books.js
+++ part8New/library/library-frontend/src/components/Books.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 
 import  { ALL_BOOKS } from '../queries'
 
+function makeArrayUnique(array) {
+  const uniqueArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (uniqueArray.indexOf(array[i]) === -1) {
+      uniqueArray.push(array[i])
+    }
+  }
+  return uniqueArray
+}
+
 const Books = (props) => {
+  const [genre, setGenre] = useState('all books')
+
   const books = useQuery(ALL_BOOKS)
+  const genres = books.data ? makeArrayUnique(books.data.allBooks.flatMap(book =>
+    book.genres  
+  ))
+  : []
+
+  const genresPlusAllBooks = genres.concat('all books')
+
 
   if (!props.show) {
     return null
@@ -14,11 +34,22 @@ const Books = (props) => {
     return <div>loading...</div>
   }
 
+  const filteredBooks = genre === 'all books'
+    ? books.data.allBooks
+    : books.data.allBooks.filter(book =>
+      book.genres.includes(genre)  
+    )
 
   return (
     <div>
       <h2>books</h2>
-
+      {genresPlusAllBooks.map(genre => 
+        <button
+          key={genre}
+          onClick={() => setGenre(genre)}
+        >{genre}
+        </button>
+      )}
       <table>
         <tbody>
           <tr>
@@ -30,7 +61,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.data.allBooks.map(book =>
+          {filteredBooks.map(book =>
             <tr key={book.title}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
