d663e05ee542128b6077ab2b0d1af7472e5163b3
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-06 11:41:06 +0200

    Bring back exercisecalculator

---
+++ part9/src/exerciseCalculator.ts
@@ -0,0 +1,69 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number
+}
+
+const calculateRating = (averageTargetDifference: number) => {
+  if (averageTargetDifference < 0.25) {
+    return {
+      success: true,
+      rating: 3,
+      ratingDescription: 'great job!'
+    };
+  }
+  else if (averageTargetDifference < 0.5) {
+    return {
+      success: false,
+      rating: 2,
+      ratingDescription: 'ok but try harder'
+    };
+  }
+  else {
+    return {
+      success: false,
+      rating: 1,
+      ratingDescription: 'not that great, step up'
+    };
+  }
+};
+
+const calculateExercises = (hours: number[], target: number): Result => {
+  const periodLength = hours.length;
+  const hoursTotal = hours.reduce(function(hoursOfCurrentDay, hoursOfNextDay) {
+    return hoursOfCurrentDay + hoursOfNextDay;
+  }, 0);
+  const average = hoursTotal / periodLength;
+  const averageTargetDifference = target - average;
+  const trainingDays = hours.filter(trainingHours => trainingHours > 0).length;
+
+  const ratings = calculateRating(averageTargetDifference);
+
+  const stats = {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: ratings.success,
+    rating: ratings.rating,
+    ratingDescription: ratings.ratingDescription,
+    target: target,
+    average: average
+  };
+
+  return stats;
+};
+
+const target = Number(process.argv[2]);
+
+const hours = [];
+
+for (let i = 3; i < process.argv.length; i++) {
+  hours.push(Number(process.argv[i]));
+}
+
+console.log(calculateExercises(hours, target));
+
+export default calculateExercises;
