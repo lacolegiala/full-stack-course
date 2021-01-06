adc1e019caa5261cbfa952b55190976a9f6b1f3e
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-06 15:22:18 +0200

    Fix eslint errors

--- part9/src/bmiCalculator.ts
+++ part9/src/bmiCalculator.ts
@@ -1,17 +1,17 @@
-const calculateBmi = (height: number, weight: number) => {
-  const bmi = weight / ((height / 100) ** 2)
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / ((height / 100) ** 2);
   if (!bmi) {
-    return 'Invalid arguments! Height and weight must be numbers greater than 0'
+    return 'Invalid arguments! Height and weight must be numbers greater than 0';
   }
   if (bmi < 18.5) {
-    return 'Underweight. Go eat a sandwich.'
+    return 'Underweight. Go eat a sandwich.';
   }
   else if (bmi < 25) {
-    return 'Healthy weight. Carry on as usual.'
+    return 'Healthy weight. Carry on as usual.';
   }
   else {
-    return 'Overweight. Go for a walk to get some vegetables.'
+    return 'Overweight. Go for a walk to get some vegetables.';
   }
-}
+};
 
-export default calculateBmi
+export default calculateBmi;
