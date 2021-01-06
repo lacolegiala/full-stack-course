cac8e3c7d8ae2af45e668fc20c66a3ae5d67864f
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-06 11:34:08 +0200

    Set up the basics and bmicalculator

---
+++ part9/src/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / ((height / 10) * (height / 10))
+  if (bmi < 18.5) {
+    return 'Underweight. Go eat a sandwich.'
+  }
+  else if (bmi < 25) {
+    return 'Healthy weight. Carry on as usual.'
+  }
+  else {
+    return 'Overweight. Go for a walk to get some vegetables.'
+  }
+}
+
+console.log(calculateBmi(158, 54))
+
+export default calculateBmi
